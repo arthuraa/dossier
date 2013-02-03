e9472f3ec34438e29bd2e6fa7fd99f3babcc5a32
Nicu Stiurca <nstiurca@seas.upenn.edu>
2013-02-03 18:41:21 -0500

    timelines form looks good

--- Handler/Timelines.hs
+++ Handler/Timelines.hs
@@ -1,8 +1,20 @@
 module Handler.Timelines where
 import Import
 
+timelineForm :: Form Timeline
+timelineForm = renderDivs $ Timeline
+  <$> areq textField "Name" Nothing
+  <*> areq textField "Description" Nothing
+  <*> areq textareaField "Contents" Nothing
+
 getTimelinesR :: Handler RepHtml
 getTimelinesR = do
+  (widget, enctype) <- generateFormPost timelineForm
+  
   defaultLayout $ do
     setTitle "Here are the available Timelines"
     $(widgetFile "timelines")
+
+postTimelinesR :: Handler RepHtml
+postTimelinesR = undefined -- do
+  -- TODO stub
